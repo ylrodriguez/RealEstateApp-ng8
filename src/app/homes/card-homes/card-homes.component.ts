e943279b58f446c1f6b396aab480d5d1f5e167bd
Yojhan <yojhanr95@gmail.com>
2020-06-11 18:15:55 -0500

    Adds list of CardHomes at HomeComponent to organize info. This list allows scrolling CardHomes component render image using padding-bottom % instead of height %, trick to be responsive Creates CardHomes Component that shows basic info of a Home

--- src/app/homes/card-homes/card-homes.component.ts
+++ src/app/homes/card-homes/card-homes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Home } from 'src/app/shared/models/home.model';
 
 @Component({
   selector: 'app-card-homes',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardHomesComponent implements OnInit {
 
+  @Input() home: Home;
+  
   constructor() { }
 
   ngOnInit() {
